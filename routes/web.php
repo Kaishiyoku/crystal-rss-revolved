ea9623d0c235e3b52a8041f3ac054243587acf03
Kaishiyoku <dmp@nfze.de>
2021-07-18 18:14:53 +0200

    added Laravel Jetstream

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
