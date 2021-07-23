dd8522827c377e7c77706e3ad42a2227615f653b
Kaishiyoku <dmp@nfze.de>
2021-07-24 00:53:11 +0200

    #2 added dark mode select inputs

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false, 'name' => null, 'value' => null, 'options' => []])
 
-{{ html()->select($name, $options, $value)->disabled($disabled)->class([$attributes->get('class'), 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50', 'border-red-800' => $errors->has($name)]) }}
+{{ html()->select($name, $options, $value)->disabled($disabled)->class([$attributes->get('class'), 'dark:bg-gray-800 rounded-md shadow-sm border-gray-300 dark:border-gray-600 focus:border-indigo-300 dark:focus:border-indigo-500 focus:ring focus:ring-indigo-200 dark:focus:ring-indigo-500 focus:ring-opacity-50', 'border-red-800' => $errors->has($name)]) }}
