2777fa2133e21b87658572d707a1311c1d93ac13
Kaishiyoku <dmp@nfze.de>
2021-07-24 01:10:51 +0200

    closed #2 added dark mode

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,7 +47,7 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 dark:text-gray-500 hover:text-gray-900 dark:hover:text-gray-400" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
