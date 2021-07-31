f3c5da81a7f8f8c4228f93e40a7611940fa2cb92
Kaishiyoku <dmp@nfze.de>
2021-07-31 15:54:59 +0200

    fixed websockets encryption configuration (7)

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => env('LARAVEL_WEBSOCKETS_USE_TLS', true),
                 'host' => '127.0.0.1',
-                'port' => env('LARAVEL_WEBSOCKETS_PORT', 6001),
+                'port' => 6001,
                 'scheme' => env('LARAVEL_WEBSOCKETS_SCHEME', 'https'),
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
