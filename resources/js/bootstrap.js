c699a3e67b5c399650ae8186ed7b4c8d7bd4890d
Kaishiyoku <dmp@nfze.de>
2021-07-31 13:40:51 +0200

    minor adjustments

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,7 +24,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: process.env.MIX_WEBSOCKETS_USE_TLS,
+    forceTLS: process.env.MIX_WEBSOCKETS_USE_TLS === 'true',
     wsHost: window.location.hostname,
     wsPort: 6001,
     enabledTransports: ['ws', 'wss'],
