c9ed847bb7382009e88808f3ce45546262d099b0
Kaishiyoku <dmp@nfze.de>
2021-08-02 00:12:15 +0200

    added the ability to disable push notifications

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,15 +13,17 @@
 
     <x-feed-list/>
 
-    @push('scripts')
-        <script type="text/javascript">
-            onDomReady(() => {
-                Echo.private(`feed-list.${userId}`)
-                    .listen('NewFeedItemsFetched', ({title, message}) => {
-                        Alpine.store('toasts').add(message, 10000);
-                        sendPushNotification(title, message, 10000, () => window.location.reload());
-                    });
-            });
-        </script>
-    @endpush
+    @if (config('app.enable_push_notifications'))
+        @push('scripts')
+            <script type="text/javascript">
+                onDomReady(() => {
+                    Echo.private(`feed-list.${userId}`)
+                        .listen('NewFeedItemsFetched', ({title, message}) => {
+                            Alpine.store('toasts').add(message, 10000);
+                            sendPushNotification(title, message, 10000, () => window.location.reload());
+                        });
+                });
+            </script>
+        @endpush
+    @endif
 </x-app-layout>
