e90922959dad524d0a89a5548fb3280641d0d15b
Kaishiyoku <dmp@nfze.de>
2021-11-01 18:46:52 +0100

    updated Composer dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 8.66.0.
+ * Generated for Laravel 8.68.1.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
