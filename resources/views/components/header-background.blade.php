379697fc68153f51fe125cdc6ab386eb1ae71ebc
Kaishiyoku <dmp@nfze.de>
2021-12-20 14:09:10 +0100

    adjusted welcome page background

---
+++ resources/views/components/header-background.blade.php
@@ -0,0 +1,9 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" style="z-index: -1;" {{ $attributes->except('style') }}>
+    <defs>
+        <linearGradient id="pathGradient" x1="0" y1="0.5" x2="1" y2="0.5">
+            <stop offset="0" stop-color="#6927ff"/>
+            <stop offset="1" stop-color="#914cd9"/>
+        </linearGradient>
+    </defs>
+    <path fill="url(#pathGradient)" fill-opacity="1" d="M0,224L60,192C120,160,240,96,360,101.3C480,107,600,181,720,192C840,203,960,149,1080,128C1200,107,1320,117,1380,122.7L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
+</svg>
