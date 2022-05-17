dc0350ec38a4439ba8bf49328d79b7ace07dc6fc
Kaishiyoku <dmp@nfze.de>
2022-05-17 11:26:16 +0200

    fixed migration

--- database/migrations/2022_04_30_160343_remove_theme_from_users_table.php
+++ database/migrations/2022_04_30_160343_remove_theme_from_users_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\QueryException;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,9 +14,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('theme');
-        });
+        try {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('theme');
+            });
+        } catch (QueryException $exception) {
+            //
+        }
     }
 
     /**
