c93224d0e269a6217c5933382b3b4e954e0f54ef
Kaishiyoku <dmp@nfze.de>
2022-10-03 11:45:26 +0200

    adjusted feed layout

--- resources/views/components/feed-item.blade.php
+++ resources/views/components/feed-item.blade.php
@@ -1,46 +1,50 @@
 @props(['feedItem'])
 
 <x-card.card class="grow">
-    <a
-        class="flex flex-col justify-between h-full group sm:hover:bg-primary-500 block transition ease-out duration-300 sm:focus:outline-none sm:focus:text-white sm:focus:bg-primary-600 rounded-md"
-        href="{{ $feedItem->url }}"
-        :class="{'opacity-40': isRead(@json($feedItem->id))}"
+    <div
+        class="flex flex-col justify-between h-full block transition ease-out duration-300 rounded-md"
+        :class="{ 'opacity-40': isRead(@json($feedItem->id)) }"
     >
         <div class="rounded-t-md">
             @if ($feedItem->has_image)
                 <x-lazy-image
                     :src="$feedItem->image_url"
                     :alt="$feedItem->title"
-                    class="group-hover:brightness-125 object-cover w-full h-72 md:h-56 rounded-t-md transition"
+                    class="object-cover w-full h-72 md:h-56 rounded-t-md transition"
                 />
             @else
-                <x-heroicon-s-photograph class="group-hover:brightness-125 fill-current text-white dark:text-gray-400 bg-gray-300 dark:bg-gray-700 w-full h-72 md:h-56 rounded-t-md transition"/>
+                <x-heroicon-s-photograph class="fill-current text-white dark:text-gray-400 bg-gray-300 dark:bg-gray-700 w-full h-72 md:h-56 rounded-t-md transition"/>
             @endif
         </div>
-        <div class="w-full px-4 py-3">
-            <div class="sm:group-hover:text-white text-2xl overflow-hidden hyphens-auto break-words" lang="{{ $feedItem->feed->language }}">{{ $feedItem->title }}</div>
-            <div class="group-hover:text-primary-600 dark:group-hover:text-primary-400 sm:group-hover:text-primary-200 w-full group-focus:text-primary-100 text-muted pt-2">
+
+        <x-card.body class="grow flex flex-col">
+            <a
+                href="{{ $feedItem->url }}"
+                lang="{{ $feedItem->feed->language }}"
+                class="text-primary-600 sm:hover:text-primary-700 hover:underline text-2xl overflow-hidden hyphens-auto break-words"
+            >
+                {{ $feedItem->title }}
+            </a>
+            <div class="w-full text-muted pt-2">
                 <div class="flex items-center">
                     @if ($feedItem->feed->favicon_url)
-                        <x-lazy-image :src="$feedItem->feed->favicon_url" class="w-4 h-4" alt="Favicon"/>
+                        <x-lazy-image :src="$feedItem->feed->favicon_url" class="w-4 h-4"/>
                     @else
                         <x-heroicon-o-photograph class="h-4 w-4"/>
                     @endif
 
-                    <div class="ml-1">{{ $feedItem->feed->name }}</div>
+                    <div class="ml-1 text-sm">{{ $feedItem->feed->name }}</div>
                 </div>
-                <div>{{ $feedItem->formatted_posted_at }}</div>
+                <div class="pt-2 text-sm">{{ $feedItem->formatted_posted_at }}</div>
             </div>
 
             @if (Auth::user()->is_feed_item_description_visible && $feedItem->description)
-                <div class="group-hover:text-primary-600 dark:group-hover:text-primary-400 sm:group-hover:text-primary-200 group-focus:text-primary-100 pt-1 text-muted overflow-hidden line-clamp-6 xl:line-clamp-3 break-all">{{ $feedItem->description }}</div>
+                <div class="grow pt-2 text-muted overflow-hidden line-clamp-6 xl:line-clamp-3 break-all">{{ $feedItem->description }}</div>
             @endif
-        </div>
 
-        <div class="mt-4">
             <button
                 type="button"
-                class="shadow-inner hover:text-white sm:group-hover:text-white group-hover:border-primary-500 sm:group-hover:border-primary-400 w-full inline-flex items-center px-4 py-4 border border-gray-200 dark:border-gray-700/50 hover:bg-primary-700 focus:bg-primary-800 focus:text-white hover:shadow-md focus:shadow-lg outline-none rounded-b-md focus:ring focus:ring-inset focus:ring-primary-400 transition"
+                class="mt-4 w-full inline-flex items-center p-4 bg-white dark:bg-primary-600 border border-gray-300 rounded-md font-semibold text-xs text-gray-600 uppercase tracking-widest shadow dark:shadow-black/25 hover:text-white hover:bg-primary-700 dark:hover:bg-primary-500 active:bg-gray-900 dark:active:bg-primary-800 focus:outline-none focus:border-gray-900 focus:ring-2 focus:ring-primary-300/75 dark:focus:ring-primary-400/75 ring-offset-2 focus:ring-primary-200 dark:focus:ring-primary-600 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition"
                 :disabled="isLoading(@json($feedItem->id))"
                 @click.prevent="toggleMarkAsRead(@json($feedItem->id))"
             >
@@ -56,6 +60,6 @@
 
                 <span>{{ __('Mark as read') }}</span>
             </button>
-        </div>
-    </a>
+        </x-card.body>
+    </div>
 </x-card.card>
