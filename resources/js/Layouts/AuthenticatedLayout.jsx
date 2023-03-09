87bb4ff6cb317abe793598c867d5793fa09edec2
Kaishiyoku <dmp@nfze.de>
2023-03-09 22:31:38 +0100

    #26 adjusted navigation dropdown toggle style

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -45,7 +45,7 @@ export default function Authenticated({auth, header, actions, withMobileSpacing 
                                         <span className="inline-flex rounded-md">
                                             <button
                                                 type="button"
-                                                className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                                className="inline-flex text-gray-500 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700/50 dark:hover:text-white px-3 py-2 rounded-md text-sm transition"
                                             >
                                                 {auth.user.name}
 
