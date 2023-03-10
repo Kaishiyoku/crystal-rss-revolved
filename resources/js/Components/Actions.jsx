9f02e66fed4d4c77cab78b3ca1b0b8a99af2be36
Kaishiyoku <dmp@nfze.de>
2023-03-10 20:30:05 +0100

    minor layout adjustments

--- resources/js/Components/Actions.jsx
+++ resources/js/Components/Actions.jsx
@@ -1,6 +1,10 @@
 import clsx from 'clsx';
 
 export default function Actions({withMobileSpacing = false, className, children}) {
+    if (!children) {
+        return null;
+    }
+
     return (
         <div className={clsx('flex justify-end pb-5', {'px-4 sm:px-0': withMobileSpacing}, className)}>
             {children}
