51478321b9ad5c40beb31dfe614fd41860a485aa
Kaishiyoku <dmp@nfze.de>
2023-03-12 00:14:34 +0100

    adjusted color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -13,6 +14,9 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                gray: colors.zinc,
             },
         },
     },
