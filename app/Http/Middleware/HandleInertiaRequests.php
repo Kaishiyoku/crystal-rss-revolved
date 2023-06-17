891057507468a38257e54a34c6a326d7694a9379
Kaishiyoku <dmp@nfze.de>
2023-06-17 12:56:51 +0200

    refactored code

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -34,6 +34,8 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'locale' => $request->getLocale(),
+            'localeValues' => collect(config('app.available_locales'))->map(fn (string $locale) => ['label' => __('common.locale.'.$locale), 'value' => $locale]),
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
                     'location' => $request->url(),
