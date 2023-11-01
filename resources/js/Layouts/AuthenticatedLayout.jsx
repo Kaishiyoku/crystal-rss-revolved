54d0dfa118c88309bbee495190134f5cb87f8aa8
Kaishiyoku <dmp@nfze.de>
2023-11-01 13:52:12 +0100

    fixed ESLint warnings

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -1,7 +1,6 @@
 import {Fragment, useEffect, useState} from 'react';
 import {Link} from '@inertiajs/react';
 import {useLaravelReactI18n} from 'laravel-react-i18n';
-import clsx from 'clsx';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Dropdown from '@/Components/Dropdown';
 import NavLink from '@/Components/NavLink';
