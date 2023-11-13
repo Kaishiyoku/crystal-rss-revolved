a3c257d9ac558565519e46053c32ef00ce8307fd
Kaishiyoku <dmp@nfze.de>
2023-11-13 20:04:28 +0100

    #34 converted TotalNumberOfFeedItemsContext to TypeScript

--- resources/js/Contexts/TotalNumberOfFeedItemsContext.js
+++
@@ -1,5 +0,0 @@
-import {createContext} from 'react';
-
-const TotalNumberOfFeedItemsContext = createContext([]);
-
-export default TotalNumberOfFeedItemsContext;
---
+++ resources/js/Contexts/TotalNumberOfFeedItemsContext.ts
@@ -0,0 +1,5 @@
+import {createContext, Context} from 'react';
+
+const TotalNumberOfFeedItemsContext: Context<number> = createContext(0);
+
+export default TotalNumberOfFeedItemsContext;
