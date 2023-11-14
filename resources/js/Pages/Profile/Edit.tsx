f5f54ec69b3427b8b4b1c8b34afc381790f2a8db
Kaishiyoku <dmp@nfze.de>
2023-11-14 19:55:31 +0100

    #34 fixed ESLint warning

--- resources/js/Pages/Profile/Edit.tsx
+++ resources/js/Pages/Profile/Edit.tsx
@@ -4,13 +4,14 @@ import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 import {Head} from '@inertiajs/react';
 import Header from '@/Components/Page/Header';
-import {PageProps} from '@/types';
+import {BasePageProps} from '@/types';
 
-export default function Edit({auth, mustVerifyEmail, status}: PageProps & { mustVerifyEmail: boolean; status: string; }) {
+export default function Edit({auth, errors, mustVerifyEmail, status}: BasePageProps & { mustVerifyEmail: boolean; status: string; }) {
     return (
         <AuthenticatedLayout
             auth={auth}
             header={<Header>Profile</Header>}
+            errors={errors}
         >
             <Head title="Profile"/>
 
