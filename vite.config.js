86cec2a9f5d0554abc021dd47b8441695704147a
Kaishiyoku <dmp@nfze.de>
2023-11-14 19:59:07 +0100

    #34 adjusted vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import i18n from 'laravel-react-i18n/vite';
@@ -6,7 +6,7 @@ import i18n from 'laravel-react-i18n/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: 'resources/js/app.tsx',
             refresh: true,
         }),
         react(),
