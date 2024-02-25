50d710f77463c095301ece9e5eac66b3441b2050
Kaishiyoku <dmp@nfze.de>
2024-02-25 20:36:39 +0100

    added TypeScript model generator tests

---
+++ tests/Feature/Console/Commands/GenerateModelsToTypeScriptTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Console\Commands\GenerateModelsToTypeScript;
+use function Pest\Laravel\artisan;
+
+it('generates TypeScript types', function () {
+    artisan(GenerateModelsToTypeScript::class)
+        ->assertSuccessful();
+
+    // check if code has been generated
+    expect(array_values(array_diff((array) scandir(config('type-script-model-generator.output_directory')), ['..', '.'])))->toBe([
+        'Category.ts',
+        'CategoryWithFeedsCount.ts',
+        'Feed.ts',
+        'FeedItem.ts',
+        'FeedWithFeedItemsCount.ts',
+        'ShortFeed.ts',
+        'ShortFeedWithFeedItemsCount.ts',
+        'User.ts',
+    ]);
+});
---
+++ tests/Feature/Services/TypeScriptModelGenerator/TypeScriptModelGeneratorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Feed;
+use App\Services\TypeScriptModelGenerator\TypeScriptModelGenerator;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Filesystem\Filesystem;
+
+it('generates types for a model', closure: function() {
+    $files = new Filesystem();
+    $customOutputDirectory = './test/resources/js/types/generated/Models';
+
+    Config::set('type-script-model-generator.output_directory', $customOutputDirectory);
+
+    (new TypeScriptModelGenerator())->generateModel(Feed::class);
+
+    expect(array_values(array_diff(scandir($customOutputDirectory), ['..', '.'])))->toBe([
+        'Feed.ts',
+        'FeedWithFeedItemsCount.ts',
+        'ShortFeed.ts',
+    ]);
+
+    $files->deleteDirectory($customOutputDirectory);
+
+    Config::set('type-script-model-generator.output_directory', null);
+});
