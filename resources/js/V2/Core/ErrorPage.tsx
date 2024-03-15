018b5917acd97740757fce9b90feb3bc181a4749
Kaishiyoku <dmp@nfze.de>
2024-03-15 21:23:45 +0100

    adjusted error page layout

--- resources/js/V2/Core/ErrorPage.tsx
+++ resources/js/V2/Core/ErrorPage.tsx
@@ -1,16 +1,20 @@
-import {isRouteErrorResponse, useAsyncError, useRouteError} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import ArrowUturnLeftSolidIcon from '@/Icons/ArrowUturnLeftSolidIcon';
+import {useLaravelReactI18n} from 'laravel-react-i18n';
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    const asyncError = useAsyncError();
+    const {t} = useLaravelReactI18n();
 
-    if (asyncError) {
-        return <div>Async error occurred: {JSON.stringify({asyncError})}</div>;
-    }
+    return (
+        <main className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8 py-12">
+            <div className="text-2xl pb-4">
+                {t('We’re sorry; an error occurred.')}
+            </div>
 
-    if (isRouteErrorResponse(error)) {
-        return <div>{error.status} {error.statusText}.</div>;
-    }
-
-    return <div>Unknown error occurred: {JSON.stringify(error)}</div>;
+            <Link to="/" className="link-secondary inline-flex space-x-2">
+                <ArrowUturnLeftSolidIcon className="size-5"/>
+                <span>{t('Back to landing page')}</span>
+            </Link>
+        </main>
+    );
 }
