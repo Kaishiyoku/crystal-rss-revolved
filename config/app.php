ba769c539ee12a15b4457823c20288d95fb15179
Kaishiyoku <dmp@nfze.de>
2024-03-22 12:50:12 +0100

    added HorizonServiceProvider to providers list

--- config/app.php
+++ config/app.php
@@ -218,6 +218,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
 
     ],
 
