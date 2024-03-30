66962cff6a2fd2edc89a9cdfc3c2c7fb693a5b7c
Shift <shift@laravelshift.com>
2024-03-30 18:46:28 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Console\Commands\CheckFeedFavicons;
+use App\Console\Commands\FetchFeedItems;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command(FetchFeedItems::class)->everyThirtyMinutes();
+Schedule::command(CheckFeedFavicons::class)->dailyAt('02:00');
+
+Schedule::command('model:prune')->daily();
+Schedule::command('telescope:prune', ['--hours' => 72])->daily();
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
