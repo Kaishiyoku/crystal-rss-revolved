bc767b77ed39b899bcef3ce6050be8e7e623e8b2
Kaishiyoku <dmp@nfze.de>
2024-06-16 21:07:32 +0200

    Revert "fixed FeedDiscovererController tests"
    This reverts commit 146ae606be717b521ddf490fd6dc0b32021c5bd8.

--- tests/Feature/Http/Controllers/FeedDiscovererControllerTest.php
+++ tests/Feature/Http/Controllers/FeedDiscovererControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Http\Controllers\Api;
+namespace Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,7 +14,7 @@ class FeedDiscovererControllerTest extends TestCase
     {
         $this->actingAs(User::factory()->create());
 
-        $this->json('post', route('api.discover-feed'), ['feed_url' => 'https://tailwindcss.com/feeds/feed.xml'])
+        $this->post(route('discover-feed'), ['feed_url' => 'https://tailwindcss.com/feeds/feed.xml'])
             ->assertJsonIsObject()
             ->assertJsonStructure([
                 'feed_url',
@@ -29,7 +29,7 @@ class FeedDiscovererControllerTest extends TestCase
     {
         $this->actingAs(User::factory()->create());
 
-        $response = $this->json('post', route('api.discover-feed'), ['feed_url' => 'https://blurha.sh/']);
+        $response = $this->post(route('discover-feed'), ['feed_url' => 'https://blurha.sh/']);
 
         $response->assertNotFound();
         static::assertSame('No feeds found.', $response->exception->getMessage());
@@ -37,15 +37,15 @@ class FeedDiscovererControllerTest extends TestCase
 
     public function test_cannot_access_as_guest(): void
     {
-        $this->json('post', route('api.discover-feed'))
-            ->assertUnauthorized();
+        $this->post(route('discover-feed'))
+            ->assertRedirect('/login');
     }
 
     public function test_connect_exception(): void
     {
         $this->actingAs(User::factory()->create());
 
-        $response = $this->json('post', route('api.discover-feed'), ['feed_url' => 'https://test.dev']);
+        $response = $this->post(route('discover-feed'), ['feed_url' => 'https://test.dev']);
 
         $response->assertUnprocessable();
         static::assertSame('The given URL is invalid.', $response->exception->getMessage());
@@ -55,7 +55,7 @@ class FeedDiscovererControllerTest extends TestCase
     {
         $this->actingAs(User::factory()->create());
 
-        $response = $this->json('post', route('api.discover-feed'), ['feed_url' => 'https://tailwindcss.com/random-page']);
+        $response = $this->post(route('discover-feed'), ['feed_url' => 'https://tailwindcss.com/random-page']);
 
         $response->assertUnprocessable();
         static::assertSame('The given URL could not be resolved.', $response->exception->getMessage());
