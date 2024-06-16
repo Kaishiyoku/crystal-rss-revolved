863b1b08cd5590ce09329ed80305818805c99ee4
Kaishiyoku <dmp@nfze.de>
2024-06-16 21:07:11 +0200

    Revert "adjusted vite.config.mjs"
    This reverts commit b727151d96fab8935b496c57849632a013dda9d5.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
 
         <!-- Scripts -->
         @viteReactRefresh
-        @vite(['resources/js/app.tsx'])
+        @vite(['resources/js/app.tsx', 'resources/js/bootstrap.ts', 'resources/css/app.css'])
     </head>
     <body class="font-sans antialiased">
         <div id="app"></div>
