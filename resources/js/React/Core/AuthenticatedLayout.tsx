51d8f1fd932411a9823a2cd5539d6ac15040767c
Kaishiyoku <dmp@nfze.de>
2024-06-16 21:12:00 +0200

    Revert "set page title properly"
    This reverts commit c3e162535e62266c60359517f6c23c9236def428.

--- resources/js/React/Core/AuthenticatedLayout.tsx
+++ resources/js/React/Core/AuthenticatedLayout.tsx
@@ -1,16 +1,7 @@
-import {Outlet, useLocation, useMatches} from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import Breadcrumbs from '@/React/Core/Breadcrumbs';
-import {useEffect} from 'react';
-import MatchWithHandle from '@/React/types/MatchWithHandle';
 
 const AuthenticatedLayout = () => {
-    const location = useLocation();
-    const match = useMatches().find((match) => match.pathname === location.pathname) as MatchWithHandle;
-
-    useEffect(() => {
-        document.querySelector('title')!.textContent = match?.handle.title;
-    }, [location]);
-
     return (
         <>
             <header className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-xl flex space-x-2">
