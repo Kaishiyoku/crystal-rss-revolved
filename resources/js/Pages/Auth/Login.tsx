c54f909ef1add40fa551c79fc17ec2285446881f
Kaishiyoku <dmp@nfze.de>
2025-01-18 12:05:35 +0100

    fixed TypeScript error

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -12,7 +12,7 @@ export default function Login({status, canResetPassword}: { status: string; canR
     const {data, setData, post, processing, errors, reset} = useForm({
         email: '',
         password: '',
-        remember: false,
+        remember: false as boolean,
     });
 
     useEffect(() => {
