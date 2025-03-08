f1ba35c24b488c293adaf0ee8ff1b1abd712367d
Kaishiyoku <dmp@nfze.de>
2025-03-08 12:37:19 +0100

    fixed TypeScript error

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -10,10 +10,10 @@ type AuthenticatedLayoutProps = BasePageProps & { header: ReactNode; breadcrumbs
     | (BasePageProps & { header?: ReactNode; breadcrumbs: Breadcrumb[] | undefined; actions?: ReactNode; children: ReactNode; });
 
 export default function Authenticated({auth, header, breadcrumbs, actions, children}: AuthenticatedLayoutProps) {
-    const {selectedFeedId, unreadFeeds} = usePage<PageProps>().props;
+    const {selectedFeedId} = usePage<PageProps>().props;
 
     return (
-        <Navigation user={auth.user} selectedFeedId={selectedFeedId} unreadFeeds={unreadFeeds}>
+        <Navigation user={auth.user} selectedFeedId={selectedFeedId}>
             {(header || breadcrumbs) && (
                 <div className="flex w-full flex-wrap items-end justify-between gap-4 border-b border-zinc-950/10 mb-8 pb-6 dark:border-white/10">
                     {header && !breadcrumbs && (
