67c908e2e36b0bb497dde9a3e1fa6133b62b2264
Kaishiyoku <dmp@nfze.de>
2025-06-13 12:31:44 +0200

    fixed asset building for blade files

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,7 +6,10 @@ import i18n from 'laravel-react-i18n/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.tsx',
+            input: [
+                'resources/js/app.tsx',
+                'resources/css/app.css',
+            ],
             refresh: true,
         }),
         react(),
